5576e6055f9f38a8903b833396069b9426eb7dc0
Till Brede <tb@symcon.de>
2019-09-30 16:07:38 +0200

    added validation test

---
+++ tests/VerbrauchZeitspanneValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class VerbrauchZeitspanneValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateVerbrauchZeitspanne(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateVerbrauchZeitspanneModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../VerbrauchZeitspanne');
+    }
+}

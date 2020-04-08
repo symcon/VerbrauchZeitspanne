cefe1482466a09700d6327b8e3f9975aad50a521
Till Brede <tb@symcon.de>
2020-04-08 10:26:36 +0200

    Shifted setting of dates
    in order to be able to trigger the same hour case

--- VerbrauchZeitspanne/module.php
+++ VerbrauchZeitspanne/module.php
@@ -130,16 +130,16 @@ include_once __DIR__ . '/timetest.php';
             $levelOfDetail = $this->ReadPropertyInteger('LevelOfDetail');
             $values = [];
             $sum = 0;
+            if ($levelOfDetail == LOD_TIME) {
+                $startDate = strtotime(date('H:i:s', $startDate), $this->getTime());
+                $endDate = strtotime(date('H:i:s', $endDate), $this->getTime());
+            }
             if ($levelOfDetail == LOD_DATE) {
                 $values = array_merge($values, AC_GetAggregatedValues($acID, $variableID, 1 /* Day */, $startDate, strtotime(date('d-m-Y', $endDate) . ' 23:59:59'), 0));
+            //Check if start/enddate are in the same hour
             } elseif (date('d.m.Y H', $startDate) == date('d.m.Y H', $endDate)) {
                 $values = array_merge($values, AC_GetAggregatedValues($acID, $variableID, 6 /* Minutes */, $startDate, $endDate, 0));
             } else {
-                if ($levelOfDetail == LOD_TIME) {
-                    $startDate = strtotime(date('H:i:s', $startDate), $this->getTime());
-                    $endDate = strtotime(date('H:i:s', $endDate), $this->getTime());
-                }
-
                 //FirstMinutes
                 $this->SendDebug('FirstMinutsStart', date('H:i:s', $startDate), 0);
                 $firstMinutesEnd = strtotime(date('H', $startDate) . ':59:59', $startDate);
